62cbd712ae3a8251ca0007a5c485f62f3096ea79
Aidar <myakkaynt228@gmail.com>
2023-05-23 09:40:07 +0300

    api gateway added

---
+++ demo-service/src/main/java/com/ms/Controller.java
@@ -0,0 +1,18 @@
+package com.ms;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+@RequiredArgsConstructor
+public class Controller {
+
+    @GetMapping()
+    public String demo(){
+        return "it works";
+    }
+}
