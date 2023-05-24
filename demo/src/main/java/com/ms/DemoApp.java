fdba2618d76850ff6336abe59f90e25d934ad723
Aidar <myakkaynt228@gmail.com>
2023-05-24 19:51:06 +0300

    auth service with jwt authorization added

--- demo/src/main/java/com/ms/DemoApp.java
+++ demo/src/main/java/com/ms/DemoApp.java
@@ -1,14 +1,13 @@
 package com.ms;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class DemoApplication {
-
+public class DemoApp {
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(DemoApp.class,args);
     }
-
 }
