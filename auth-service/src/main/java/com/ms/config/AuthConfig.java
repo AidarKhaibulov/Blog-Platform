4092f30783fc060950cb5ca7fdafa4bcaeb49060
Aidar <myakkaynt228@gmail.com>
2023-06-03 21:41:29 +0300

    architecture diagram edited

--- auth-service/src/main/java/com/ms/config/AuthConfig.java
+++ auth-service/src/main/java/com/ms/config/AuthConfig.java
@@ -2,10 +2,24 @@ package com.ms.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
 @Configuration
+@EnableWebSecurity
 public class AuthConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/auth/register", "/auth/token", "/auth/validate").permitAll()
+                .and()
+                .build();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
