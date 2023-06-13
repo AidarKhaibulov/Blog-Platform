b0778f55c5c7ff825bb171e4585f7bbdabe66e65
Aidar <myakkaynt228@gmail.com>
2023-06-13 19:43:16 +0300

    1)added tests for auth service 2)added blog manage service 3)added article manage service

--- auth-service/src/test/java/com/ms/AuthServiceTests.java
+++ auth-service/src/test/java/com/ms/AuthServiceTests.java
@@ -2,60 +2,73 @@ package com.ms;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ms.controller.AuthController;
-import com.ms.dto.ApiResponse;
 import com.ms.dto.RegisterRequest;
 import com.ms.dto.UserDto;
 import com.ms.service.AuthService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class AuthServiceTests {
+class AuthServiceTests {
 
     @Autowired
     private MockMvc mockMvc;
     @InjectMocks
     private AuthController authController;
-    @Mock
+    @MockBean
     private AuthService authService;
+    @MockBean
+    private AuthenticationManager authenticationManager;
     private ObjectMapper objectMapper;
 
     @BeforeEach
     public void setup() {
         objectMapper = new ObjectMapper();
     }
+
     @Test
     @WithMockUser
-    public void testAddNewUser() throws Exception {
+    void registerUser_withValidData() throws Exception {
         RegisterRequest registerRequest = new RegisterRequest();
         registerRequest.setUsername("testuser");
         registerRequest.setPassword("password");
         registerRequest.setEmail("test@example.com");
 
         UserDto userDto = new UserDto("testuser");
-        given(authService.register(registerRequest)).willReturn(userDto);
+        when(authService.register(registerRequest)).thenReturn(userDto);
 
-        MvcResult result = mockMvc.perform(post("/auth/register")
+        mockMvc.perform(post("/auth/register")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(registerRequest)))
-                .andExpect(status().isOk())
-                .andReturn();
+                .andExpect(status().isOk());
+
+        verify(authService, times(1)).register(registerRequest);
+    }
+
+    @Test
+    @WithMockUser
+    void registerUser_withInvalidPasswordLength() throws Exception {
+        RegisterRequest registerRequest = new RegisterRequest();
+        registerRequest.setUsername("testuser");
+        registerRequest.setPassword("1234");
+        registerRequest.setEmail("test@example.com");
+
+        mockMvc.perform(post("/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(registerRequest)))
+                .andExpect(status().isBadRequest());
     }
 }
